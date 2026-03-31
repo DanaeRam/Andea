5718176ccf3c7f822cea55646ef9d9c821506c22
Dana <danae.ramirezrodriguez@gmail.com>
2026-03-30 19:02:46 -0600

    Creación de plantilla para las escenas del mundo de lecto/escritura

---
+++ Assets/MundoLE/ScriptsLE/FlagTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    // public QuizManager quizManager;
+
+    // private bool activated = false;
+
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (activated) return;
+
+    //     if (collision.CompareTag("Player"))
+    //     {
+    //         activated = true;
+    //         quizManager.ShowQuestion();
+    //     }
+    // }
+}
