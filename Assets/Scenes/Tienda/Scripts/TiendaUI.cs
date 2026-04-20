2f99659bf9a59c32af3d3b629f3ba56e57a8c952
A01754094 <A01754094@tec.mx>
2026-04-19 18:53:13 -0600

    Sonidos y Botón Menú
    Se agregó sonido a las escenas iniciales de cada mundo. Además se agregaron dos botones, uno desde mainscena a tienda y el segundo de tienda a mainscene.

--- Assets/Scenes/Tienda/Scripts/TiendaUI.cs
+++ Assets/Scenes/Tienda/Scripts/TiendaUI.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TiendaUI : MonoBehaviour
 {
     public GameObject fondoPrincipal;
     public GameObject panelDialogo;
     public GameObject panelArticulos;
+    public GameObject ButtonMain;
 
     void Start()
     {
@@ -22,23 +24,28 @@ public class TiendaUI : MonoBehaviour
     {
         if (fondoPrincipal != null) fondoPrincipal.SetActive(false);
         if (panelDialogo != null) panelDialogo.SetActive(true);
+        if(ButtonMain != null) ButtonMain.SetActive(false);
+
     }
 
     public void CerrarDialogo()
     {
         if (panelDialogo != null) panelDialogo.SetActive(false);
         if (fondoPrincipal != null) fondoPrincipal.SetActive(true);
+        if(ButtonMain != null) ButtonMain.SetActive(true);
     }
 
     public void AbrirArticulos()
     {
         if (panelDialogo != null) panelDialogo.SetActive(false);
         if (panelArticulos != null) panelArticulos.SetActive(true);
+        if(ButtonMain != null) ButtonMain.SetActive(false);
     }
 
     public void CerrarArticulos()
     {
         if (panelArticulos != null) panelArticulos.SetActive(false);
         if (fondoPrincipal != null) fondoPrincipal.SetActive(true);
+        if(ButtonMain != null) ButtonMain.SetActive(true);
     }
 }
